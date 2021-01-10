db6b849d37338da5b7285571b3ea99cfe518f7d7
detalikota <detalikota@gmail.com>
2021-01-10 20:29:40 +0300

    yeah

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView, ListView
+from .models import Post
 
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'pages/home.html'
+    context_object_name = 'post_list'
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
 # Create your views here.
