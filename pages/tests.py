18c6b79d92db6d88528bc189002a9d329fb06f98
detalikota <detalikota@gmail.com>
2021-01-11 08:30:58 +0300

    another commit

--- pages/tests.py
+++ pages/tests.py
@@ -18,7 +18,6 @@ class HomePageViewTest(TestCase):
     def test_view_url_exists_at_proper_location(self):
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
-
     def test_view_url_by_name(self):
         resp = self.client.get(reverse('home'))
         self.assertEqual(resp.status_code, 200)
